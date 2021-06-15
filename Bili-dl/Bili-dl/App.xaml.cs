82f776a39da01989cdce92e22cef63af742f7d52
瑄 <shanboxuan@me.com>
2021-06-15 22:52:21 +0100

    Bump version to 0.9.3

--- Bili-dl/Bili-dl/App.xaml.cs
+++ Bili-dl/Bili-dl/App.xaml.cs
@@ -15,7 +15,7 @@ namespace Bili_dl
     {
         public App()
         {
-            this.Resources.Add("Version", "v0.9.2-alpha");
+            this.Resources.Add("Version", "v0.9.3-alpha");
 
             this.DispatcherUnhandledException += (object sender, DispatcherUnhandledExceptionEventArgs e) =>
             {
